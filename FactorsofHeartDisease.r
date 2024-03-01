220382e94ced35e0f10a99163083e8e9c63ed785
Melissa Mattos <99051624+mmattos2000@users.noreply.github.com>
2024-02-29 16:06:30 -0800

    Create FactorsofHeartDisease.r

---
+++ FactorsofHeartDisease.r
@@ -0,0 +1,25 @@
+# Factors of Heart Disease Research Project
+
+Heart disease stands as a leading cause of death in the United States, claiming approximately 659,000 lives each year, with one person succumbing to it every 36 seconds. Given its prevalence, it's likely that most individuals have encountered someone in their social circle affected by this condition. Motivated by this widespread impact, I embarked on a research project centered around exploring the potential relationship between body mass index (BMI), sleep duration, and the risk of heart disease.
+
+## Motivation
+Understanding the contributing factors to heart disease risk is crucial for prevention and intervention strategies. By investigating the correlation between BMI, sleep habits, and heart disease, we aim to shed light on lifestyle choices that may influence susceptibility to the disease. Armed with this knowledge, we can adopt healthier habits and advocate for informed decision-making among others.
+
+## Methodology
+Utilizing logistic regression, a predictive modeling technique, we sought to assess the likelihood of heart disease occurrence based on BMI and sleep duration. The logistic regression model employs explanatory variables derived from these key factors to compute a response variable, facilitating the prediction and analysis of heart disease risk.
+
+## Dataset
+The original dataset comprises responses from a survey conducted among 400,000 adults in the United States, capturing information on heart disease status, BMI, and average sleep duration. For this project, we have curated a cleaned dataset containing the relevant columns: 'Heart Disease', 'BMI', and 'Sleep Time'.
+
+- **Original Dataset**: [Kaggle - Personal Key Indicators of Heart Disease](https://www.kaggle.com/datasets/kamilpytlak/personal-key-indicators-of-heart-disease)
+- **Cleaned Dataset**: [Google Sheets - Heart Disease Research Data](https://docs.google.com/spreadsheets/d/1S8l53VDxbnpeuWB-tze9_HD5m9x4UVErm5c-ppX-hJI/edit#gid=0)
+
+## Research Questions
+To gather insights into the relationship between BMI, sleep duration, and heart disease risk, the survey posed the following questions to participants:
+
+1. Do you have heart disease?
+2. What is your BMI?
+3. On average, how many hours do you sleep?
+
+## Conclusion
+This research endeavor aims to elucidate the potential links between lifestyle factors and heart disease risk, with a focus on BMI and sleep duration. By unraveling these associations, we aspire to empower individuals with actionable insights to mitigate their risk and foster heart-healthy behaviors. Through continued exploration and dissemination of this knowledge, we strive to make meaningful strides towards reducing the burden of heart disease in our communities.
